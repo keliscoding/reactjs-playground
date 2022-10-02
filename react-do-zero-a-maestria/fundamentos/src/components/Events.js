9194abbdec89631b92e45733066af7a1be238f23
zam0k <kawokkun@gmail.com>
2022-10-02 13:14:43 -0300

    função de renderização

--- react-do-zero-a-maestria/fundamentos/src/components/Events.js
+++ react-do-zero-a-maestria/fundamentos/src/components/Events.js
@@ -3,11 +3,23 @@ const Events = () => {
         console.log(e)
     }
 
+    const renderSomething = (x) => {
+        if(x) {
+            return <h1>Renderizando isso!</h1>
+        }
+        return <h1>Também renderizo isso!!!</h1>
+    }
+
     return (
         <div>
             <div>
                 <button onClick={handleMyEvent}>Clique aqui!</button>
             </div>
+            <div>
+                <button onClick={() => console.log("Clicou")}>Clique aqui também</button>
+            </div>
+            {renderSomething(true)}
+            {renderSomething(false)}
         </div>
     );
 }
