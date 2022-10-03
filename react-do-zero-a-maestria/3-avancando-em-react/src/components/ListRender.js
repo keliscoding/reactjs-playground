e3debb643f279233bc04f7e575d51d6fd6d9a0a5
zam0k <kawokkun@gmail.com>
2022-10-03 08:18:36 -0300

    previous state

--- react-do-zero-a-maestria/3-avancando-em-react/src/components/ListRender.js
+++ react-do-zero-a-maestria/3-avancando-em-react/src/components/ListRender.js
@@ -3,13 +3,29 @@ import { useState } from 'react'
 const ListRender = () => {
     const [list] = useState(["Matheus", "Pedro", "Tobias", "Maria"])
 
+    const [users, setUsers] = useState([
+        {id: 1, name: "Matheus", age: 31},
+        {id: 2, name: "João", age: 28},
+        {id: 3, name: "Pedro", age: 44},
+    ])
+
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4);
+
+        setUsers((prevUsers) => {
+            return prevUsers.filter(user => randomNumber !== user.id)
+        });
+    }
+
   return (
     <div>
         <ul>
-            {list.map((item, index) => (
-                <li key={index}>{item}</li>
+            {/* é uma má prática usar o index como key */}
+            {users.map(({id, name, age}) => (
+                <li key={id}>name: {name}, age: {age}</li>
             ) )}
         </ul>
+        <button onClick={deleteRandom}>Delete random user</button>
     </div>
   )
 }
