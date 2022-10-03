516a981d01360eea8425863ba17d4bdc1f2c9a3e
zam0k <kawokkun@gmail.com>
2022-10-03 13:31:38 -0300

    css de componentes

--- react-do-zero-a-maestria/4-css/src/App.js
+++ react-do-zero-a-maestria/4-css/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
+      {/* CSS global */}
       <h1>React com CSS</h1>
+      {/* CSS de componente */}
+      <MyComponent/>
+      <p>O estilo está vazando pra fora do componente</p>
     </div>
   );
 }
