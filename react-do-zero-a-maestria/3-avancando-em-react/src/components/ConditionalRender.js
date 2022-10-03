af868a3ee588776b832697c2d933f4f3d225920d
zam0k <kawokkun@gmail.com>
2022-10-03 08:47:46 -0300

    if e else no jsx

--- react-do-zero-a-maestria/3-avancando-em-react/src/components/ConditionalRender.js
+++ react-do-zero-a-maestria/3-avancando-em-react/src/components/ConditionalRender.js
@@ -3,11 +3,20 @@ import { useState } from 'react';
 const ConditionalRender = () => {
     const [x] = useState(false);
 
+    const [name, setName] = useState("Matheus");
+
   return (
     <div>
         <h1>Isso será exibido?</h1>
         {x && <p>Se x for true, sim!</p>}
         {!x && <p>Agora x é falso!</p>}
+        <h2>If Ternário</h2>
+        {name === "João"? (
+            <div>O nome é João.</div>
+        ) : (
+            <div>O nome não é João.</div>
+        )}
+        <button onClick={() => setName("João")}>Mudar nome pra João</button>
     </div>
   )
 }
