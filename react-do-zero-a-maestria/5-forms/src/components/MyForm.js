1f233d07c13e4727a3496b85a06dbf544aa0e85f
zam0k <kawokkun@gmail.com>
2022-10-04 07:38:56 -0300

    gerenciamento de dados de input

--- react-do-zero-a-maestria/5-forms/src/components/MyForm.js
+++ react-do-zero-a-maestria/5-forms/src/components/MyForm.js
@@ -1,15 +1,28 @@
 import './MyForm.css';
 
-import React from 'react';
+import { useState } from 'react';
 
 const MyForm = () => {
+   // 3 - gerenciamento de dados
+
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+
+    //isso aqui não é a maneira mais adequada de lidar com evento em formulario
+    const handleChange = (e) => {
+        setName(e.target.value);
+    }
+
+    console.log(name);
+
   return (
     <div>
         {/* 1 - criação de form */}
         <form>
             <div>
                 <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" placeholder='Digite seu nome'/>
+                <input type="text" name="name" placeholder='Digite seu nome'
+                onChange={handleChange}/>
             </div>
             <input type="submit" value="Enviar"/>
             {/* 2 - label envolvendo input  */}
