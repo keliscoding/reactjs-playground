7e6fb220af8fff43658f19b22e1ecd27fb76fa7d
zam0k <kawokkun@gmail.com>
2022-10-04 09:03:00 -0300

    controlled inputs

--- react-do-zero-a-maestria/5-forms/src/components/MyForm.js
+++ react-do-zero-a-maestria/5-forms/src/components/MyForm.js
@@ -2,35 +2,42 @@ import './MyForm.css';
 
 import { useState } from 'react';
 
-const MyForm = () => {
+const MyForm = ({user}) => {
    // 3 - gerenciamento de dados
 
-    const [name, setName] = useState()
-    const [email, setEmail] = useState()
+    const [name, setName] = useState(user ? user.name : '')
+    const [email, setEmail] = useState(user ? user.email : '')
 
     //isso aqui não é a maneira mais adequada de lidar com evento em formulario
     const handleChange = (e) => {
         setName(e.target.value);
     }
-
-    console.log(name);
-    console.log(email);
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("enviando o formulario")
+        console.log(name);
+        console.log(email);
+    }
 
   return (
     <div>
+        {/* 6 - controlled input */}
+        {/* 5 - envio de form */}
         {/* 1 - criação de form */}
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">Nome:</label>
                 <input type="text" name="name" placeholder='Digite seu nome'
-                onChange={handleChange}/>
+                onChange={handleChange} value={name}/>
             </div>
-            <input type="submit" value="Enviar"/>
             {/* 2 - label envolvendo input  */}
             <label>
                 <span>E-mail:</span>
-                <input type="email" name="email" placeholder='Digite o seu e-mail' onChange={(e) => setEmail(e.target.value)}/>
+                {/* 4 - simplificação da manipulação de state */}
+                <input type="email" name="email" placeholder='Digite o seu e-mail' onChange={(e) => setEmail(e.target.value)} value={email}/>
             </label>
+            <input type="submit" value="Enviar"/>
         </form>
     </div>
   )
