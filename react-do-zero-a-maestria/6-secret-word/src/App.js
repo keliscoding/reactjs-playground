f47a01f9fa63167bc29cc9923c1c82495a5d9e85
zam0k <kawokkun@gmail.com>
2022-10-04 11:02:57 -0300

    criando componentes do projeto

--- react-do-zero-a-maestria/6-secret-word/src/App.js
+++ react-do-zero-a-maestria/6-secret-word/src/App.js
@@ -1,10 +1,34 @@
+//CSS
 import './App.css';
+// React
+import { useState } from 'react';
+
+//data
+
+import { wordsList } from './data/words';
+
+//components
+import Game from './components/Game';
+import GameOver from './components/GameOver';
 import StartScreen from './components/StartScreen';
 
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"}
+]
+
 function App() {
+
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+
   return (
     <div className="App">
-      <StartScreen/>
+      {gameStage === "start" && <StartScreen/>}
+      {gameStage === "game" && <Game/>}
+      {gameStage === "end" && <GameOver/>}
     </div>
   );
 }
