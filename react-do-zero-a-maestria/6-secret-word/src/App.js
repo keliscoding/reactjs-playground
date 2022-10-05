2317e00c6b0e4c8faee020f3e9b14e52725c41f0
zam0k <kawokkun@gmail.com>
2022-10-05 10:55:29 -0300

    processando letras válidas e inválidas

--- react-do-zero-a-maestria/6-secret-word/src/App.js
+++ react-do-zero-a-maestria/6-secret-word/src/App.js
@@ -65,7 +65,32 @@ function App() {
 
   // process the letter input
   const verifyLetter = (letter) => {
-    console.log(letter);
+
+    const normalizedLetter = letter.toLowerCase();
+
+    //checa se a letra já foi utilizada
+    if(guessedLetters.includes(normalizedLetter) 
+    || wrongLetters.includes(normalizedLetter)) {
+      return;
+    }
+
+    //push guessed letter or remove a guess
+
+    if(letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => 
+      [...actualGuessedLetters, 
+        normalizedLetter
+      ])
+    }
+    else {
+      setWrongLetters((actualWrongLetters) => 
+      [...actualWrongLetters, 
+        normalizedLetter
+      ])
+    }
+
+    console.log(guessedLetters);
+    console.log(wrongLetters);
     
     // setGameStage(stages[2].name)
   };
