b43e41433fc5f3203b0444fcd7a310d54fd8e4aa
zam0k <kawokkun@gmail.com>
2022-10-05 10:10:21 -0300

    consumindo states do jogo

--- react-do-zero-a-maestria/6-secret-word/src/components/Game.js
+++ react-do-zero-a-maestria/6-secret-word/src/components/Game.js
@@ -1,31 +1,40 @@
 import './Game.css';
 
-const Game = ({verifyLetter}) => {
+const Game = ({verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score}) => {
   return (
     <div className='game'>
       <p className='points'>
-        <span>Pontuação: 000</span>
+        <span>Pontuação: {score}</span>
       </p>
       <h1>Adivinhe a palavra:</h1>
       <h3 className='tip'>
-        Dica sobre a palavra: <span>Dica...</span>
+        Dica sobre a palavra: <span>{pickedCategory}</span>
       </h3>
-        <div className="wordContainer">
-          <span className="letter">A</span>
-          <span className="blankSquare"></span>
-        </div>
-        <div className="letterContainer">
-          <p>Tente adivinhar uma letra da palavra:</p>
-          <form>
-            <input type="text" name="letter" maxLength="1" required/>
-            <button>Jogar!</button>
-          </form>
-        </div>
-        <div className="wrongLettersContainer">
-          <p>Letras já utilizadas:</p>
-          <span>a,</span>
-          <span>b,</span>
-        </div>
+
+      <p>Você ainda tem {guesses} tentativa(s).</p>
+
+      <div className="wordContainer">
+        {letters.map((letter, i)=> 
+          guessedLetters.includes(letter) ? (
+            <span key={i} className="letter">{letter}</span>
+          ) : (
+            <span key={i} className="blankSquare"></span>
+          )
+        )}
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required/>
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        {wrongLetters.map((letter, i) => 
+          <span key={i}>{letter}, </span>
+        )}
+      </div>
     </div>
   )
 }
