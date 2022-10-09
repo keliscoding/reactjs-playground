740831ba6a7b4115eb98fb1e10f463db4e907850
zam0k <kawokkun@gmail.com>
2022-10-09 09:18:17 -0300

    estado de loading

--- react-do-zero-a-maestria/7-req-http/src/App.js
+++ react-do-zero-a-maestria/7-req-http/src/App.js
@@ -13,7 +13,7 @@ function App() {
   const [products, setProducts] = useState([]);
 
   // 4 - custom hook
-  const { data: items, httpConfig } = useFetch(url);
+  const { data: items, httpConfig, loading } = useFetch(url);
 
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
@@ -66,13 +66,15 @@ function App() {
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
-      <ul>
+      {/* 6 - loading */}
+      { loading && <p>carregando dados...</p>}
+      {!loading && <ul>
         {items && items.map((product) => (
           <li key={product.id}>
             {product.name} - R${product.price}</li>
           ))
         }
-      </ul>
+      </ul>}
       <div className="add-product">
         <form onSubmit={handleSubmit}>
           <label>
--- react-do-zero-a-maestria/7-req-http/src/hooks/useFetch.js
+++ react-do-zero-a-maestria/7-req-http/src/hooks/useFetch.js
@@ -11,6 +11,10 @@ export const useFetch = (url) => {
     const [method, setMethod] = useState(null);
     const [callFetch, setCallFetch] = useState(false);
 
+    //6 - loading
+    const [loading, setLoading] = useState(false);
+
+
     const httpConfig = (data, method) => {
         if(method === 'POST') {
             setConfig({
@@ -27,9 +31,15 @@ export const useFetch = (url) => {
 
     useEffect(() => {
         const fetchData = async () => {
+
+            // 6 - loading
+            setLoading(true);
+
             const res = await fetch(url);
             const json = await res.json();
             setData(json);
+
+            setLoading(false);
         }
 
         fetchData();
@@ -49,5 +59,5 @@ export const useFetch = (url) => {
         httpRequest();
     }, [config, method, url])
 
-    return { data, httpConfig };
+    return { data, httpConfig, loading };
 };
