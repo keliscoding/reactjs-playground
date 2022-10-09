e1b5f8d68d1ed1ee6876150828ddbf750912fb6c
zam0k <kawokkun@gmail.com>
2022-10-09 07:33:58 -0300

    resgatando dados da API

--- react-do-zero-a-maestria/7-req-http/src/App.js
+++ react-do-zero-a-maestria/7-req-http/src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
+const url = "http://localhost:3000/products"
+
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  // 1 - resgatando dados
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch(url)
+        .then((res) => res.json());
+
+      setProducts(data);
+    }
+
+    fetchData().catch(console.error);
+
+  }, []);
+
+  console.log(products);
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R${product.price}</li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
