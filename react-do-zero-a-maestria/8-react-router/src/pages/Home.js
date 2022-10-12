2b1821395d0988b899434df74d87ad09d58d10d5
zam0k <kawokkun@gmail.com>
2022-10-12 10:18:49 -0300

    carregando dados

--- react-do-zero-a-maestria/8-react-router/src/pages/Home.js
+++ react-do-zero-a-maestria/8-react-router/src/pages/Home.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { useFetch } from '../hooks/useFetch';
+import "./Home.css";
 
 const Home = () => {
+
+  // 3- carregamento de dados
+
+  const url = "http://localhost:3000/products"
+
+  const { data: items, loading, error } = useFetch(url);
+
   return (
-    <div>Home</div>
+    <div>
+      <h1>Produtos</h1>
+      {error && <p>{error}</p>}
+      <ul className="products">
+        {items && items.map(item => (
+          <li key={item.id}>
+            <h2>{item.name}</h2>
+            <p>R$: {item.price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
