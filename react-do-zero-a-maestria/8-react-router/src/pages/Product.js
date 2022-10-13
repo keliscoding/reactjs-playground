4d2c5cbda95518d3805a88bcb01e56cc901c3ddc
zam0k <kawokkun@gmail.com>
2022-10-13 08:55:51 -0300

    carregamento dinamico de dados

--- react-do-zero-a-maestria/8-react-router/src/pages/Product.js
+++ react-do-zero-a-maestria/8-react-router/src/pages/Product.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
 import { useParams } from 'react-router-dom';
+import { useFetch } from '../hooks/useFetch';
 
 function Product() {
     //4 - rota dinamica
     const {id} = useParams();
 
+    //5- carregamento dinamico
+    const url = "http://localhost:3000/products/" + id;
+    const {data, loading, error} = useFetch(url);
+
+    console.log(data);
+
   return (
     <>
-        <p>Id do produto: {id}</p>
+        {error && <p>{error}</p>}
+        {loading && <p>loading....</p>}
+        {!loading && <div>
+            <h2>{data.name}</h2>
+            <p>price: R$ {data.price},00</p>
+            </div>}
     </>
   )
 }
