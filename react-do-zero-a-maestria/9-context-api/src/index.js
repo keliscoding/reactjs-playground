7682fe4eccfbacfea83d02242d010eef45e1a8bf
zam0k <kawokkun@gmail.com>
2022-10-14 08:02:44 -0300

    criando provider

--- react-do-zero-a-maestria/9-context-api/src/index.js
+++ react-do-zero-a-maestria/9-context-api/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+
+import { CounterContextProvider } from './context/CounterContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* 2 - criando provider */}
+    <CounterContextProvider>
+      <App />
+    </CounterContextProvider>
   </React.StrictMode>
 );
 
