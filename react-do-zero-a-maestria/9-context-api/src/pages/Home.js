1f2a617f4dcab93a119c4e061e33e1de5ecb1536
zam0k <kawokkun@gmail.com>
2022-10-14 08:48:54 -0300

    alterando contexto

--- react-do-zero-a-maestria/9-context-api/src/pages/Home.js
+++ react-do-zero-a-maestria/9-context-api/src/pages/Home.js
@@ -1,6 +1,8 @@
 import { useContext } from 'react';
 import { CounterContext } from '../context/CounterContext';
 
+import ChangeCounter from '../components/ChangeCounter';
+
 const Home = () => {
 
   const { counter } = useContext(CounterContext);
@@ -9,6 +11,8 @@ const Home = () => {
     <div>
       <h1>Home:</h1>
       <p>Valor do contador: {counter}</p>
+      {/* 3 - alterando o valor do contexto */}
+      <ChangeCounter/>
     </div>
   )
 }
