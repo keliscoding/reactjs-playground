af26391489517eb37d83ab1482c39d6d6b27731e
zam0k <kawokkun@gmail.com>
2022-10-15 10:36:50 -0300

    adicionando componente de navbar

--- react-do-zero-a-maestria/10-miniblog/src/App.js
+++ react-do-zero-a-maestria/10-miniblog/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 import About from './pages/About/About';
 import Home from './pages/Home/Home';
 
@@ -8,12 +10,14 @@ const App = () => {
   return (
     <div className="App">
         <BrowserRouter>
+        <Navbar/>
           <div className="container">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
             </Routes>
           </div>
+          <Footer/>
         </BrowserRouter>
     </div>
   );
