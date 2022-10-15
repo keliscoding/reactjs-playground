d02106d18e8c836c4e48c6d36490d959c105c1be
zam0k <kawokkun@gmail.com>
2022-10-15 10:13:48 -0300

    configurando o react router

--- react-do-zero-a-maestria/10-miniblog/src/App.js
+++ react-do-zero-a-maestria/10-miniblog/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 
-function App() {
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './pages/About/About';
+import Home from './pages/Home/Home';
+
+const App = () => {
   return (
     <div className="App">
-      <h1>MiniBlog</h1>
+        <BrowserRouter>
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
     </div>
   );
 }
