8c379ed61c2dbaef61832fdab068688e3963ce1a
zam0k <kawokkun@gmail.com>
2022-10-17 13:40:56 -0300

    css dos detalhes do post

--- react-do-zero-a-maestria/10-miniblog/src/components/PostDetails.js
+++ react-do-zero-a-maestria/10-miniblog/src/components/PostDetails.js
@@ -6,11 +6,11 @@ const PostDetails = ({post}) => {
     
   return (
 
-    <div>
+    <div className={styles.post_detail}>
         <img src={post.image} alt={post.title} />
         <h2>{post.title}</h2>
-        <p>{post.createdBy}</p>
-        <div>
+        <p className={styles.createdby}>{post.createdBy}</p>
+        <div className={styles.tags}>
             {post.tagsArray.map((tag) => (
                 <p key={tag}><span>#</span>{tag}</p>
             ))}
