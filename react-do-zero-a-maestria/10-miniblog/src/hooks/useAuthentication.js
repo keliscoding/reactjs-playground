c0dcd3442974e58ae006f57bef802fd24653d589
zam0k <kawokkun@gmail.com>
2022-10-17 09:01:17 -0300

    função criar post

--- react-do-zero-a-maestria/10-miniblog/src/hooks/useAuthentication.js
+++ react-do-zero-a-maestria/10-miniblog/src/hooks/useAuthentication.js
@@ -1,5 +1,5 @@
 import {
-    createUserWithEmailAndPassword, getAuth, signOut, updateProfile
+    createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut, updateProfile
 } from 'firebase/auth';
 
 import { useEffect, useState } from 'react';
@@ -64,6 +64,34 @@ export const useAuthentication = () => {
         signOut(auth);
     }
 
+    // login - sign in
+    const login = async(data) => {
+        checkIfIsCancelled();
+
+        setLoading(true);
+        setError(false);
+
+        try {
+            await signInWithEmailAndPassword(auth, data.email, data.password);
+        } catch (error) {
+
+            let systemErrorMessage;
+            
+            if(error.message.includes("user-not-found")) {
+                systemErrorMessage = "Usuário não encontrado.";
+            }else if(error.message.includes("wrong-password")){
+                systemErrorMessage = "Senha incorreta.";
+            } else {
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde."
+            }
+
+            setError(systemErrorMessage);
+        } finally {
+            setLoading(false);
+        }
+
+    }
+
     useEffect(() => {
         return() => setCancelled(true);
     }, []);
@@ -73,6 +101,7 @@ export const useAuthentication = () => {
         createUser,
         error,
         loading,
-        logout
+        logout,
+        login
     }
 }
--- react-do-zero-a-maestria/10-miniblog/src/pages/CreatePost/CreatePost.js
+++ react-do-zero-a-maestria/10-miniblog/src/pages/CreatePost/CreatePost.js
@@ -1,8 +1,67 @@
 import styles from './CreatePost.module.css';
 
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuthValue } from '../../context/AuthContext';
+
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div><h1>CreatePost</h1></div>
+    <div className={styles.create_post}>
+      <h2>Create Post</h2>
+      <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Título:</span>
+          <input type="text" 
+            name="title" 
+            required
+            placeholder="Pense num bom título..."
+            onChange={(e) => setTitle(e.target.value)} 
+            value={title}/>
+        </label>
+        <label>
+          <span>URL da imagem:</span>
+          <input type="text" 
+            name="image" 
+            required
+            placeholder="Insira uma imagem que representa o seu post"
+            onChange={(e) => setImage(e.target.value)} 
+            value={image}/>
+        </label>
+        <label>
+          <span>Descrição:</span>
+          <textarea type="text" 
+            name="body" 
+            required
+            placeholder="Insira o conteúdo do post1"
+            onChange={(e) => setBody(e.target.value)} 
+            value={body}/>
+        </label>
+        <label>
+          <span>Tags:</span>
+          <input type="text" 
+            name="tags" 
+            required
+            placeholder="Insira as tags separadas por vírgulas"
+            onChange={(e) => setTags(e.target.value)} 
+            value={tags}/>
+        </label>
+        <button className='btn'>Register</button>
+        {/* {!loading && <button className='btn'>Register</button>}
+        {loading && <button className='btn' disabled>Wait...</button>}
+        {error && <p className='error'>{error}</p>} */}
+      </form>
+    </div>
   )
 }
 
