f5adfdc892f64c33ef529484977a1ef91a0b6be8
zam0k <kawokkun@gmail.com>
2022-10-18 09:16:06 -0300

    excluindo post

--- react-do-zero-a-maestria/10-miniblog/src/pages/Dashboard/Dashboard.js
+++ react-do-zero-a-maestria/10-miniblog/src/pages/Dashboard/Dashboard.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 
 import { useAuthValue } from '../../context/AuthContext';
 import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+import { useDeleteDocument } from '../../hooks/useDeleteDocument';
 
 const Dashboard = () => {
   
@@ -15,10 +16,8 @@ const Dashboard = () => {
   // post do usuario
 
   const {documents: posts, loading } = useFetchDocuments("posts", null, uid);
-  
-  const deleteDocument = (id) => {
 
-  }
+  const { deleteDocument } = useDeleteDocument("posts");
   
   if(loading) {
     return <p>Carregando...</p>;
