e0040de83738b555a93314ab72b79847870e542a
zam0k <kawokkun@gmail.com>
2022-10-27 15:49:04 -0300

    validações de criação de usuário

--- react-do-zero-a-maestria/12-reactgram/backend/middlewares/handleValidation.js
+++ react-do-zero-a-maestria/12-reactgram/backend/middlewares/handleValidation.js
@@ -3,7 +3,7 @@ const { validationResult } = require("express-validator");
 const validate = (req, res, next) => {
   const errors = validationResult(req);
 
-  if (errors.isEmpty) {
+  if (errors.isEmpty()) {
     return next();
   }
 
