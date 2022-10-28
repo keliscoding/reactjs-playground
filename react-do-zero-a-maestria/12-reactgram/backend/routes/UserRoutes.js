a935b468ca666f052633559b746335f2d37fe8ee
zam0k <kawokkun@gmail.com>
2022-10-28 09:36:17 -0300

    resgatando usuário pelo id

--- react-do-zero-a-maestria/12-reactgram/backend/routes/UserRoutes.js
+++ react-do-zero-a-maestria/12-reactgram/backend/routes/UserRoutes.js
@@ -6,7 +6,8 @@ const {
   register,
   login,
   getCurrentUser,
-  update
+  update,
+  getUserById,
 } = require("../controllers/UserController");
 
 // Middlewares
@@ -32,5 +33,6 @@ router.put(
   imageUpload.single("profileImage"),
   update
 );
+router.get("/:id", getUserById);
 
 module.exports = router;
