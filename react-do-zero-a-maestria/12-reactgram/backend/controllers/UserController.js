111ec00e789c621a4f43ad2f89fbdc20649473d0
zam0k <kawokkun@gmail.com>
2022-10-28 07:44:12 -0300

    registro do usuario

--- react-do-zero-a-maestria/12-reactgram/backend/controllers/UserController.js
+++ react-do-zero-a-maestria/12-reactgram/backend/controllers/UserController.js
@@ -15,8 +15,38 @@ const generateToken = (id) => {
 
 // Register user and sign in
 
-const register = (req, res) => {
-    res.send("Registro")
+const register = async (req, res) => {
+    const {name, email, password} = req.body;
+
+    //check if user already exists
+    const user = await User.findOne({email});
+
+    if (user) {
+        res.status(422).json({errors: ["Email já cadastrado."]})
+        return
+    }
+
+    // Generate password hash
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // Create user
+    const newUser = await User.create({
+        name, 
+        email,
+        password: passwordHash
+    })
+
+    // if user was created successfully return the token
+    if(!newUser){
+        res.status(422).json({errors: ["Houve um erro, por favor tente mais tarde."]})
+        return
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id)
+    })
 }
 
 module.exports = { 
