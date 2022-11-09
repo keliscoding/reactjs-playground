2f68281642b179b16f366078188b888e99c34832
zam0k <kawokkun@gmail.com>
2022-11-09 09:30:10 -0300

    atualização de foto

--- react-do-zero-a-maestria/12-reactgram/backend/controllers/PhotoController.js
+++ react-do-zero-a-maestria/12-reactgram/backend/controllers/PhotoController.js
@@ -105,10 +105,41 @@ const getPhotoById = async (req, res) => {
     return res.status(200).json(photo);
 }
 
+// update photo
+
+const updatePhoto = async (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    const reqUser = req.user;
+    const photo = await Photo.findById(id);
+
+    //check if photo exists
+    if(!photo){
+        return res.status(404).json({errors: ["Foto não encontrada!"]})
+    }
+
+    //check if photo belongs to user
+    if(!photo.userId.equals(reqUser._id)) {
+        return res.status(422).json({errors: ["Ocorreu um erro, por favor tente novamente mais tarde."]});
+    }
+
+    if(title) {
+        photo.title = title;
+    }
+
+    await photo.save();
+
+    return res.status(200).json({
+        photo,
+        message: "Foto atualizada com sucesso!"
+    })
+};
+
 module.exports = {
   insertPhoto,
   deletePhoto,
   getAllPhotos,
   getUserPhotos,
-  getPhotoById
+  getPhotoById,
+  updatePhoto
 };
