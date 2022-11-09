681408502aa96ba87b090fb24ff0da3ebe1466e4
zam0k <kawokkun@gmail.com>
2022-11-09 08:17:15 -0300

    excluindo fotos

--- react-do-zero-a-maestria/12-reactgram/backend/controllers/PhotoController.js
+++ react-do-zero-a-maestria/12-reactgram/backend/controllers/PhotoController.js
@@ -29,11 +29,48 @@ const insertPhoto = async (req, res) => {
     res.status(422).json({
       errors: ["Houve um problema, por favor tente novamente mais tarde."],
     });
+    return;
   }
 
   res.status(201).json(newPhoto);
 };
 
+// Remove a photo from DB
+const deletePhoto = async (req, res) => {
+  const { id } = req.params;
+
+  const reqUser = req.user;
+
+  try {
+    const photo = await Photo.findById(mongoose.Types.ObjectId(id));
+
+    // check if photo exists
+
+    if (!photo) {
+      return res.status(422).json({ errors: ["Foto não encontrada!"] });
+    }
+
+    // check if photo belongs to user
+
+    if (!photo.userId.equals(reqUser._id)) {
+      return res.status(422).json({
+        errors: ["Ocorreu um erro, por favor tente novamente mais tarde."],
+      });
+    }
+
+    await Photo.findByIdAndDelete(photo._id);
+
+    return res
+      .status(200)
+      .json({ id: photo._id, message: "Foto excluída com sucesso." });
+  } catch (error) {
+    return res.status(404).json({
+        errors: ["Foto não encontrada!"],
+      });
+  }
+};
+
 module.exports = {
   insertPhoto,
+  deletePhoto,
 };
--- react-do-zero-a-maestria/12-reactgram/backend/routes/PhotoRoutes.js
+++ react-do-zero-a-maestria/12-reactgram/backend/routes/PhotoRoutes.js
@@ -1,17 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 //Controller
 
-const { insertPhoto } = require("../controllers/PhotoController")
+const { insertPhoto, deletePhoto } = require("../controllers/PhotoController");
 
 //Middlewares
-const { photoInsertValidation } = require("../middlewares/photoValidation")
+const { photoInsertValidation } = require("../middlewares/photoValidation");
 const authGuard = require("../middlewares/authGuard");
 const validate = require("../middlewares/handleValidation");
 const { imageUpload } = require("../middlewares/imageUpload");
 
 //Routes
-router.post("/", authGuard, imageUpload.single("image"), photoInsertValidation(), validate, insertPhoto);
+router.post(
+  "/",
+  authGuard,
+  imageUpload.single("image"),
+  photoInsertValidation(),
+  validate,
+  insertPhoto
+);
+router.delete("/:id", authGuard, deletePhoto);
 
 module.exports = router;
