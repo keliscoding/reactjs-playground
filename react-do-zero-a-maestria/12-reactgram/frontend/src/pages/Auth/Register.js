de1205ef1ec6a098d02a0bbed79885ecfd209e6d
zam0k <kawokkun@gmail.com>
2022-11-10 09:22:00 -0300

    criando formulario de cadastro

--- react-do-zero-a-maestria/12-reactgram/frontend/src/pages/Auth/Register.js
+++ react-do-zero-a-maestria/12-reactgram/frontend/src/pages/Auth/Register.js
@@ -1,9 +1,32 @@
-import './Auth.css';
+import "./Auth.css";
+
+// Components
+import { Link } from "react-router-dom";
+
+// Hooks
+import { useState, useEffect } from "react";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-export default Register
+  return (
+    <div>
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome"/>
+        <input type="email" placeholder="Email"/>
+        <input type="password" placeholder="Senha"/>
+        <input type="password" placeholder="Confirme a senha"/>
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já tem conta? <Link to="/login">Clique aqui.</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Register;
