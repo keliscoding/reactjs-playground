bb1f05158b881bae8a0bcfb0f4d1e2b283f04d77
zam0k <kawokkun@gmail.com>
2022-11-10 09:16:37 -0300

    css navbar e footer

--- react-do-zero-a-maestria/12-reactgram/frontend/src/App.js
+++ react-do-zero-a-maestria/12-reactgram/frontend/src/App.js
@@ -1,31 +1,33 @@
-import './App.css';
+import "./App.css";
 
 //Router
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 //Pages
-import Home from './pages/Home/Home'
-import Login from './pages/Auth/Login'
-import Register from './pages/Auth/Register'
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
 
 //Components
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/register" element={<Register/>} />
-        </Routes>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
         <Footer />
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
