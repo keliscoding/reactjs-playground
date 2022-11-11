e4f70b38935c07f7482a9497c0cfa276b42f8432
zam0k <kawokkun@gmail.com>
2022-11-11 13:30:01 -0300

    slice do login

--- react-do-zero-a-maestria/12-reactgram/frontend/src/slices/authSlice.js
+++ react-do-zero-a-maestria/12-reactgram/frontend/src/slices/authSlice.js
@@ -31,6 +31,22 @@ export const logout = createAsyncThunk("auth/logout", async () => {
   await authService.logout();
 });
 
+//sign in an user
+export const login = createAsyncThunk(
+  //entidade/ação
+  "auth/login",
+  async (user, thunkAPI) => {
+    const data = await authService.login(user);
+
+    //check for errors
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+  }
+);
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -62,6 +78,21 @@ export const authSlice = createSlice({
         state.loading = false;
         state.success = true;
         state.error = null;
+        state.user = null;
+      })
+      .addCase(login.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
         state.user = null;
       });
   },
