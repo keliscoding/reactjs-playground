193ef10cce9ab78fee25adc0811e73c7ba2be87c
zam0k <kawokkun@gmail.com>
2022-11-12 09:00:11 -0300

    preenchendo formulario de edicao com dados do usuario

--- react-do-zero-a-maestria/12-reactgram/frontend/src/pages/EditProfile/EditProfile.js
+++ react-do-zero-a-maestria/12-reactgram/frontend/src/pages/EditProfile/EditProfile.js
@@ -1,6 +1,45 @@
 import "./EditProfile.css";
 
+import { uploads } from "../../utils/config";
+
+// Hooks
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// Redux
+import { profile, resetMessage } from "../../slices/userSlice";
+
+// Components
+import Message from "../../components/Message/Message";
+
 const EditProfile = () => {
+  const dispatch = useDispatch();
+
+  const { user, message, error, loading } = useSelector((state) => state.user);
+
+  // states
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [bio, setBio] = useState("");
+  const [previewImage, setPreviewImage] = useState("");
+
+  // load user data
+  useEffect(() => {
+    dispatch(profile());
+  }, [dispatch]);
+
+  // fill the form with user data
+  useEffect(() => {
+    if(user) {
+        setName(user.name);
+        setEmail(user.email);
+        setBio(user.bio);
+    }
+  }, [user])
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
@@ -13,19 +52,26 @@ const EditProfile = () => {
       </p>
       {/*preview da imagem*/}
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Nome"/>
-        <input type="email" placeholder="E-email" disabled/>
+        <input
+          type="text"
+          placeholder="Nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name || ""}
+        />
+        <input type="email" placeholder="E-email" value={email || ""}disabled />
         <label>
-            <span>Imagem do Perfil:</span>
-            <input type="file" />
+          <span>Imagem do Perfil:</span>
+          <input type="file" />
         </label>
         <label>
-            <span>Bio:</span>
-            <input type="text" placeholder="Descrição do perfil"/>
+          <span>Bio:</span>
+          <input type="text" placeholder="Descrição do perfil" onChange={(e) => setBio(e.target.value)}
+          value={bio || ""}/>
         </label>
         <label>
-            <span>Quer alterar sua senha?</span>
-            <input type="password" placeholder="Digite sua nova senha"/>
+          <span>Quer alterar sua senha?</span>
+          <input type="password" placeholder="Digite sua nova senha" onChange={(e) => setPassword(e.target.value)}
+          value={password || ""}/>
         </label>
         <input type="submit" value="Atualizar" />
       </form>
