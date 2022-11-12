60e9d1843cf7257136c56646394728841c7df18d
zam0k <kawokkun@gmail.com>
2022-11-12 14:07:37 -0300

    Formulario de postar foto

--- react-do-zero-a-maestria/12-reactgram/frontend/src/pages/Profile/Profile.js
+++ react-do-zero-a-maestria/12-reactgram/frontend/src/pages/Profile/Profile.js
@@ -22,28 +22,54 @@ const Profile = () => {
   const { user, loading } = useSelector((state) => state.user);
   const { user: userAuth } = useSelector((state) => state.auth);
 
-  // photo
+  // new form and edit form refs
+  const newPhotoForm = useRef();
+  const editPhotoForm = useRef();
 
   // load user data
   useEffect(() => {
-    dispatch(getUserDetails(id))
+    dispatch(getUserDetails(id));
   }, [dispatch, id]);
 
-  if(loading) {
-    return <p>Carregando...</p>
+  if (loading) {
+    return <p>Carregando...</p>;
   }
 
-  return <div id="profile">
-    <div className="profile-header">
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div id="profile">
+      <div className="profile-header">
         {user.profileImage && (
-            <img src={`${uploads}/users/${user.profileImage}`} alt={user.name} />
+          <img src={`${uploads}/users/${user.profileImage}`} alt={user.name} />
         )}
         <div className="profile-description">
-            <h2>{user.name}</h2>
-            <p>{user.bio}</p>
+          <h2>{user.name}</h2>
+          <p>{user.bio}</p>
         </div>
+      </div>
+      {id === userAuth._id && (
+        <>
+          <div className="new-photo" ref={newPhotoForm}>
+            <h3>Compartilhe algum momento seu:</h3>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Título para a foto:</span>
+                    <input type="text" placeholder="Insira um título"/>
+                </label>
+                <label>
+                    <span>Imagem:</span>
+                    <input type="file"/>
+                </label>
+                <input type="submit" value="Postar" />
+            </form>
+          </div>
+        </>
+      )}
     </div>
-  </div>;
+  );
 };
 
 export default Profile;
