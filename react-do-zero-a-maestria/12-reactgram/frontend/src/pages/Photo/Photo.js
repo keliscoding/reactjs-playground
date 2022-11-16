a95e4b3ffb0ee5cff1a3307ef4da46dd3124f252
zam0k <kawokkun@gmail.com>
2022-11-16 09:14:02 -0300

    finalizando funcionalidade de comentário

--- react-do-zero-a-maestria/12-reactgram/frontend/src/pages/Photo/Photo.js
+++ react-do-zero-a-maestria/12-reactgram/frontend/src/pages/Photo/Photo.js
@@ -15,7 +15,7 @@ import { useParams } from "react-router-dom";
 import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
 
 // redux
-import { getPhoto, like } from "../../slices/photoSlice";
+import { getPhoto, like, comment } from "../../slices/photoSlice";
 import LikeContainer from "../../components/LikeContainer/LikeContainer";
 
 const Photo = () => {
@@ -30,14 +30,32 @@ const Photo = () => {
   );
 
   // comentários
+  const [commentText, setCommentText] = useState("");
 
   // load photo data
   useEffect(() => {
     dispatch(getPhoto(id));
   }, [dispatch, id]);
 
+  // handle a like
   const handleLike = () => {
     dispatch(like(photo._id));
+
+    resetMessage();
+  };
+
+  // insert a comment
+  const handleComment = (e) => {
+    e.preventDefault();
+
+    const commentData = {
+      comment: commentText,
+      id: photo._id,
+    };
+
+    dispatch(comment(commentData));
+
+    setCommentText("");
 
     resetMessage();
   };
@@ -49,10 +67,43 @@ const Photo = () => {
   return (
     <div id="photo">
       <PhotoItem photo={photo} />
-      <LikeContainer photo={photo} user={user} handleLike={handleLike}/>
+      <LikeContainer photo={photo} user={user} handleLike={handleLike} />
       <div className="message-container">
         {error && <Message msg={error} type="error" />}
         {message && <Message msg={message} type="success" />}
+      </div>
+      <div className="comments">
+        {photo.comments && (
+          <>
+            <h3>Comentários: {photo.comments.length}</h3>
+            <form onSubmit={handleComment}>
+              <input
+                type="text"
+                placeholder="Insira o seu comentário..."
+                onChange={(e) => setCommentText(e.target.value)}
+                value={commentText || ""}
+              />
+              <input type="submit" value="Enviar" />
+            </form>
+            {photo.comments.length === 0 && <p>Não há comentários...</p>}
+              {photo.comments.map((comment) => (
+                <div className="comment" key={comment.comment}>
+                  <div className="author">
+                    {comment.userImage && (
+                      <img
+                        src={`${uploads}/users/${comment.userImage}`}
+                        alt={comment.userName}
+                      />
+                    )}
+                    <Link to={`/users/${comment.userId}`}>
+                      <p>{comment.userName}</p>
+                    </Link>
+                  </div>
+                  <p>{comment.comment}</p>
+                </div>
+              ))}
+          </>
+        )}
       </div>
     </div>
   );
