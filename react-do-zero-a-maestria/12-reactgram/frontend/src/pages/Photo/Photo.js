548783a58fa438f0a365c7a2d764233df0b88cae
zam0k <kawokkun@gmail.com>
2022-11-16 08:29:54 -0300

    implementando mensagem nas fotos

--- react-do-zero-a-maestria/12-reactgram/frontend/src/pages/Photo/Photo.js
+++ react-do-zero-a-maestria/12-reactgram/frontend/src/pages/Photo/Photo.js
@@ -12,6 +12,7 @@ import PhotoItem from "../../components/PhotoItem/PhotoItem";
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
 
 // redux
 import { getPhoto, like } from "../../slices/photoSlice";
@@ -20,6 +21,9 @@ import LikeContainer from "../../components/LikeContainer/LikeContainer";
 const Photo = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
+
+  const resetMessage = useResetComponentMessage(dispatch);
+
   const { user } = useSelector((state) => state.auth);
   const { photo, loading, error, message } = useSelector(
     (state) => state.photo
@@ -34,6 +38,8 @@ const Photo = () => {
 
   const handleLike = () => {
     dispatch(like(photo._id));
+
+    resetMessage();
   };
 
   if (loading) {
@@ -44,6 +50,10 @@ const Photo = () => {
     <div id="photo">
       <PhotoItem photo={photo} />
       <LikeContainer photo={photo} user={user} handleLike={handleLike}/>
+      <div className="message-container">
+        {error && <Message msg={error} type="error" />}
+        {message && <Message msg={message} type="success" />}
+      </div>
     </div>
   );
 };
